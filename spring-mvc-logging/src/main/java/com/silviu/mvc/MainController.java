266b8f01ae9c05b674e5a875b834545d836edb8a
Rada Silviu <rada.silviu@softparc.eu>
2018-11-05 17:29:16 +0200

    simple implementation to see how configuration is made without boot .here i'am using WebApplicationInitializer

---
+++ spring-mvc-logging/src/main/java/com/silviu/mvc/MainController.java
@@ -0,0 +1,18 @@
+package com.silviu.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+	@ResponseBody
+	@RequestMapping("/")
+	public String message()
+	{
+		return "Hello World";
+	}
+	
+}
---
+++ spring-mvc-logging/src/main/java/com/silviu/mvc/WebConfig.java
@@ -0,0 +1,10 @@
+package com.silviu.mvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan("com.silviu.mvc")
+public class WebConfig {
+
+}
