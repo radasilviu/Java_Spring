d16a71bb27a9e1b9a207b8fa4bb9f6e2631e39dc
Rada Silviu <rada.silviu@softparc.eu>
2018-11-14 09:49:45 +0200

    Spring Chat App - add classes for model

---
+++ ChatApp/src/main/java/com/sprin/chatapp/ChatApp/model/ChatParticipant.java
@@ -0,0 +1,37 @@
+package com.sprin.chatapp.ChatApp.model;
+
+public class ChatParticipant {
+	
+	private String fistName;
+	private String lastName;
+	private String shortName;
+	private String participantType;
+	
+	public String getFistName() {
+		return fistName;
+	}
+	public void setFistName(String fistName) {
+		this.fistName = fistName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getShortName() {
+		return shortName;
+	}
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+	public String getParticipantType() {
+		return participantType;
+	}
+	public void setParticipantType(String participantType) {
+		this.participantType = participantType;
+	}
+	
+	
+
+}
