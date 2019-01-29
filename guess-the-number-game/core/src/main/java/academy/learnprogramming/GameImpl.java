f65fb69fc8bd0efa25d74f3b8e94b5ee2a3f8bcd
Rada Silviu <rada.silviu@softparc.eu>
2019-01-29 10:00:52 +0200

    add lambok for cleanup code

--- guess-the-number-game/core/src/main/java/academy/learnprogramming/GameImpl.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -1,5 +1,9 @@
 package academy.learnprogramming;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +12,13 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+@Slf4j
+@Getter
 @Component
 public class GameImpl implements Game {
 
-    // == constants ==
-    private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
-
     // == fields ==
+    @Getter(AccessLevel.NONE)
     private final NumberGenerator numberGenerator;
 
     private final int guessCount;
@@ -28,12 +32,15 @@ public class GameImpl implements Game {
         this.guessCount = guessCount;
     }
 
+
     private int number;
-    private int guess;
     private int smallest;
     private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange = true;
+
+    @Setter
+    private int guess;
 
     // == init ==
     @PostConstruct
@@ -53,40 +60,6 @@ public class GameImpl implements Game {
     }
 
     // == public methods ==
-    @Override
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public int getGuess() {
-        return guess;
-    }
-
-    @Override
-    public void setGuess(int guess) {
-        this.guess = guess;
-    }
-
-    @Override
-    public int getSmallest() {
-        return smallest;
-    }
-
-    @Override
-    public int getBiggest() {
-        return biggest;
-    }
-
-    @Override
-    public int getRemainingGuesses() {
-        return remainingGuesses;
-    }
-
-    @Override
-    public int getGuessCount() {
-        return guessCount;
-    }
 
     @Override
     public void check() {
@@ -104,11 +77,6 @@ public class GameImpl implements Game {
         }
 
         remainingGuesses--;
-    }
-
-    @Override
-    public boolean isValidNumberRange() {
-        return validNumberRange;
     }
 
     @Override
--- guess-the-number-game/core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
+++ guess-the-number-game/core/src/main/java/academy/learnprogramming/NumberGeneratorImpl.java
@@ -1,16 +1,19 @@
 package academy.learnprogramming;
 
+import lombok.AccessLevel;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Getter
 @Component
 public class NumberGeneratorImpl implements NumberGenerator {
 
-    // == fields ==
+    // == fields =
+    @Getter(AccessLevel.NONE)
     private final Random random = new Random();
-
 
     private final int maxNumber;
 
@@ -32,13 +35,5 @@ public class NumberGeneratorImpl implements NumberGenerator {
         return random.nextInt(maxNumber - minNumber) + minNumber;
     }
 
-    @Override
-    public int getMaxNumber() {
-        return maxNumber;
-    }
 
-    @Override
-    public int getMinNumber() {
-        return minNumber;
-    }
 }
